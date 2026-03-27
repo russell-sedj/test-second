d9fcc7fedc75d401522ad22eb89c0b7a64ceed5d
Russell-Sedj <russelldossouyovo@gmail.com>
2026-03-27 02:25:16 +0100

    add demarches route to application routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { Contact } from './pages/contact/contact';
 import { Home } from './pages/home/home';
 import { Mairie } from './pages/mairie/mairie';
 import { Services } from './pages/services/services';
+import { Demarches } from './pages/demarches/demarches';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'mairie', component: Mairie },
   { path: 'services', component: Services },
   { path: 'actualites', component: Actualites },
+  { path: 'demarches', component: Demarches },
   { path: 'contact', component: Contact },
   {
     path: 'admin',
